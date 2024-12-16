f3e9b4d129ea9c39d0f1db3161fa81d7883d2eba
Francien Plat <f.w.plat@student.rug.nl>
2024-12-16 11:50:08 +0100

    csv links to upload data

--- 01-avgSnowmelt_avgPeak.R
+++ 01-avgSnowmelt_avgPeak.R
@@ -3,3 +3,8 @@ remove(list=ls())
 
 # load libraries
 library(tidyverse)
+
+#load data
+insect_data<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTq7-xuTeasMbBQ366icvMvTduhKvMTsuk89mFN0lR_UzHqs67CdXmuDgs-NEuZNeBHZsmMy9LpM4Sz/pub?output=csv")
+snowmelt_data<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQsQW1IpTBbS7PK4z8e3BvXjWHCJYST2puqkeq0cOgILjYtCHj3JNV5DfwgfcJxpktqnFo-PveIs49T/pub?output=csv")
+
