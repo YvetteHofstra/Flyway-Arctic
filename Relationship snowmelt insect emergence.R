3e2eae24f66ff269f826a2dee7eb39074f7b2421
Francien Plat <f.w.plat@student.rug.nl>
2024-12-17 14:13:13 +0100

    data wrangling and first models

--- Relationship snowmelt insect emergence.R
+++ Relationship snowmelt insect emergence.R
@@ -6,17 +6,56 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 # install.packages("lme4")
-# install.packages("lmerTest")
-library(lme4)  # for linear mixed models
+install.packages("lmerTest")
+#library(lme4)  # for linear mixed models
 library(lmerTest)  # and testing their significance
 
 insect<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTq7-xuTeasMbBQ366icvMvTduhKvMTsuk89mFN0lR_UzHqs67CdXmuDgs-NEuZNeBHZsmMy9LpM4Sz/pub?output=csv")
 snowmelt<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQsQW1IpTBbS7PK4z8e3BvXjWHCJYST2puqkeq0cOgILjYtCHj3JNV5DfwgfcJxpktqnFo-PveIs49T/pub?output=csv")
+
+##################################################
+#Calculates average snow melt date for each year
+snowpeak<- snowmelt %>% group_by(Year)%>%
+  summarize(AveregeSnowMeltday = mean(Snowmelt))
+#Calculates the cumulative sum of insect biomass for each year
+output <- insect %>%
+  filter(year >= 2019) %>%
+  ungroup() %>%
+  group_by(year, doy) %>%
+  summarize(DailyBiomass = sum(Biomass), .groups = "drop") %>%
+  group_by(year) %>%
+  arrange(doy) %>%
+  mutate(runningtotal = cumsum(DailyBiomass)) %>%
+  select(year, InsectPeakday = doy, runningtotal)
+# calculates what half the insect biomass is yearly
+halfvalue <- insect %>%
+  filter(year >= 2019) %>%
+  group_by(year) %>%
+  summarize(
+    YearlyBiomass = sum(Biomass),
+    HalfYearlyBiomass = YearlyBiomass / 2
+  )
+#combines the above 2 data frames
+combined <- output %>%
+  inner_join(halfvalue, by = "year")
+#finds the data when half the insect biomass has emerged (peak insect emergence)
+result <- combined %>%
+  filter(runningtotal < HalfYearlyBiomass) %>%
+  rename(Year = year)%>%
+  group_by(Year) %>%
+  slice_max(runningtotal)
+
+#creates a dataframe with the snowmelt data
+plotting <- result %>%
+  inner_join(snowpeak, by = "Year")%>%
+  mutate(Difference = InsectPeakday - AveregeSnowMeltday)
                    
 ########## Trying some models ##########
-m1<- lmer(InsectPeakday ~ AveregeSnowMeltday + (1|Year), data = plotting)
+m1<- lm(InsectPeakday ~ AveregeSnowMeltday, data = plotting)
+summary(m1)
 
-
+m2<-glm(InsectPeakday ~ AveregeSnowMeltday, data = plotting, family = "poisson")
+summary(m2)
 
 
                   
