449b42b83ab4bc5acc329c832e4314e8a9211a9f
Francien Plat <f.w.plat@student.rug.nl>
2024-12-17 13:59:36 +0100

    start of script

---
+++ Relationship snowmelt insect emergence.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+insect<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTq7-xuTeasMbBQ366icvMvTduhKvMTsuk89mFN0lR_UzHqs67CdXmuDgs-NEuZNeBHZsmMy9LpM4Sz/pub?output=csv")
+snowmelt<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQsQW1IpTBbS7PK4z8e3BvXjWHCJYST2puqkeq0cOgILjYtCHj3JNV5DfwgfcJxpktqnFo-PveIs49T/pub?output=csv")
+                   
+                   
